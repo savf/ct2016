5ba7101d911869f0950f993e6fe1921fe62c1316
Stephan Mannhart <stephan.mannhart@googlemail.com>
2016-03-29 21:45:08 +0200

    Created maven project with fat jar packaging and made prototype runnable on single host

--- src/main/java/ch/uzh/csg/p2p/Node.java
+++ src/main/java/ch/uzh/csg/p2p/Node.java
@@ -12,7 +12,6 @@ import net.tomp2p.connection.Bindings;
 import net.tomp2p.dht.FutureGet;
 import net.tomp2p.dht.PeerBuilderDHT;
 import net.tomp2p.dht.PeerDHT;
-import net.tomp2p.futures.BaseFuture;
 import net.tomp2p.futures.BaseFutureListener;
 import net.tomp2p.futures.FutureBootstrap;
 import net.tomp2p.futures.FutureDiscover;
@@ -22,7 +21,6 @@ import net.tomp2p.storage.Data;
 
 public class Node {
 	
-	private final int PORT = 4000;
 	private final String KEY = "test";
 	
 	private Logger log;
@@ -30,29 +28,27 @@ public class Node {
 	private PeerDHT peer;
 	
 	
-	public Node(int nodeId, String knownIP) throws IOException{
+	public Node(int nodeId, int localPort, String knownIP, int knownPort) throws IOException{
 		
 		log = LoggerFactory.getLogger("Node with id: "+nodeId);
-
 		
-		createPeer(nodeId);
+		createPeer(nodeId, localPort);
 		
 		//if knownIP == null; this is first node in network and cannot connect to others
 		if(knownIP != null){
-			connectToNode(knownIP);
+			connectToNode(knownIP, knownPort);
 			FutureGet futureGet = peer.get(Number160.createHash(KEY)).start();
-			futureGet.addListener(new BaseFutureListener<BaseFuture>() {
+			futureGet.addListener(new BaseFutureListener<FutureGet>() {
 
 				public void exceptionCaught(Throwable arg0) throws Exception {
 					// TODO Auto-generated method stub
 				}
 
-				public void operationComplete(BaseFuture baseFuture) throws Exception {
-					if(baseFuture.isSuccess()){
-						//TODO change to logger
-						log.info(((FutureGet)baseFuture).data().object().toString());
-					}else{
-						log.error("BaseFutur not success: "+baseFuture.failedReason());
+				public void operationComplete(FutureGet future) throws Exception {
+					if(future.isSuccess() && future.data() != null) {
+						log.info(future.data().object().toString());
+					} else {
+						log.error("FutureGet was unsuccessful: " + future.failedReason());
 					}
 					
 				}
@@ -64,17 +60,24 @@ public class Node {
 		}
 	}
 	
-	private void createPeer(int nodeId) throws IOException{
+	private void createPeer(int nodeId, int localPort) throws IOException{
 		Bindings b = new Bindings().listenAny();
-    	peer = new PeerBuilderDHT(new PeerBuilder(new Number160(nodeId)).ports(PORT).bindings(b).start()).start();
+    	peer = new PeerBuilderDHT(new PeerBuilder(new Number160(nodeId)).ports(localPort).bindings(b).start()).start();
 	}
 
-	private void connectToNode(String knownIP) throws UnknownHostException{
+	private void connectToNode(String knownIP, int knownPort) throws UnknownHostException{
 		InetAddress address = Inet4Address.getByName(knownIP);
-		FutureDiscover futureDiscover = peer.peer().discover().inetAddress(address).ports(PORT).start();
+		FutureDiscover futureDiscover = peer.peer().discover().inetAddress(address).ports(knownPort).start();
 		futureDiscover.awaitUninterruptibly();
-		FutureBootstrap futureBootstrap = peer.peer().bootstrap().inetAddress(address).ports(PORT).start();
+		FutureBootstrap futureBootstrap = peer.peer().bootstrap().inetAddress(address).ports(knownPort).start();
 		futureBootstrap.awaitUninterruptibly();
+	}
+	
+	public void shutdown(){
+		log.info("Shutting down gracefully.");
+		if(peer != null) {
+			peer.shutdown();
+		}
 	}
 	
 }
